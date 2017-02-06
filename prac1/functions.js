e6bc52bb386eca2e292204cfd2d81c5e45298463
Muravyova <amurav@merann.ru>
2017-02-06 13:25:38 +0300

    test commit

---
+++ prac1/functions.js
@@ -0,0 +1,27 @@
+/**
+ * Created by amurav on 06.02.2017.
+ */
+
+
+
+
+function myMap(arr, func){
+
+    return function(){
+
+
+    }
+
+}
+
+function myReduce(arr){}
+
+function myThrottle(arr){}
+
+function myDebounce(func, time){
+
+    var state = true;
+
+}
+
+

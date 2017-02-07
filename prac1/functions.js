030744d7dba11b7d09f5747aa0df685e150c3a07
Muravyova <amurav@merann.ru>
2017-02-07 21:11:59 +0300

    throttle map reduce ready

--- prac1/functions.js
+++ prac1/functions.js
@@ -8,6 +8,19 @@ function func(x){
     return x*x;
 }
 
+var count = 0;
+var delay = 5000;
+var time = 20000;
+function func2() {
+    count++;
+   return console.log("throttle function is called " + count);
+}
+
+
+function func3() {
+     return console.log("debounce function is called");
+}
+
 function myMap(arr, func){
     var result = [];
 
@@ -18,54 +31,88 @@ function myMap(arr, func){
         })();
     }
 
-
-    console.log();
-
     return result;
 
 }
 console.log(myMap(arr,func));
 
-function myReduce(arr, func){
+
+
+function myReduce(arr, func) {
 
     var result = 0;
-    for(var i=0, len = arr.length; i<len; i++){
-        if(!arr[i]) continue;
-        result +=(function(){
-            func.call(this, arr[i]);
-        });
+
+    for (var i = 0; i < arr.length; i++) {
+        if (!arr[i]) continue;
+       // console.log(result);
+        result += (function () {
+            return func.call(this, arr[i]);
+        })();
     }
-    console.log(result.toString());
     return result;
-
 }
-console.log(myReduce(arr, func()));
 
-function myThrottle(func, delay){
+console.log(myReduce(arr, func));
 
+var delay = 5000;
+function func2() {
+    count++;
+    return console.log("throttle function is called " + count);
+}
+
+
+function myThrottle(func2, delay){
+
+    var flag = 1;
+    setTimeout(function() { flag = 1;},delay);
+    //Возвращает функцию, которая вызывает исходную не чаще чем один раз в n миллисекунд
     return function(){
-        var args = arguments;
-        var ctx = this;
-        setTimeout(function(){
-            func.apply(ctx, args);
-        }, delay);
-    }
+        if(flag) {
+            var args = arguments;
+            var ctx = this;
+            (function(){func2.apply(ctx, args)})();
+            flag = 0;
+        }
+
+
+    };
 }
+
+
+
+
+
+
+
+var trottle = myThrottle(func2, delay);
+
+var checkInterval = setInterval(function(){
+    trottle();
+}, 1000);
+
+setTimeout(function(){
+    clearInterval(checkInterval);
+}, 7000);
+
+
+
+
+
 
 
 function myDebounce(func, time){
     var state = true;
 
-    return function(){
+    return (function(){
         if(!state) return;
         func.apply(this, arguments);
         state = false;
         setTimeout(function(){
             state = true;
         }, time);
-    }
+    })();
 
 }
-
+console.log(myDebounce(func3, delay));
 
 

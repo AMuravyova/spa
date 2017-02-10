14e0f8763bb37cdc47e3c1c793ef9d8f941d0112
Muravyova <amurav@merann.ru>
2017-02-10 18:14:49 +0300

    all steps done

---
+++ prac1/scope3.js
@@ -0,0 +1,92 @@
+/**
+ * Created by amurav on 10.02.2017.
+ */
+
+function Scope(){
+
+    this.$$watchers = [];
+    this.$watch = function (propertyFn, listenerFn) {
+        var watcher = {
+            property: propertyFn,
+            listener: listenerFn,
+            old: null
+
+    };
+        this.$$watchers.push(watcher);
+    };
+
+
+    this.$digestOnce = function(){
+
+        var dirty = false;
+
+        for (var i = 0; i < this.$$watchers.length; i++) {
+
+            if (this.$$watchers[i].listener){
+
+                if(this.$$watchers[i].old!=this.$$watchers[i].property()){
+                    dirty = true;
+                    this.$$watchers[i].listener(this.$$watchers[i].property(),this.$$watchers[i].old,this);
+                    this.$$watchers[i].old = this.$$watchers[i].property();
+                }
+
+            }
+        }
+        return dirty;
+
+    }
+
+    this.$digest = function() {
+        var notClean = 0;
+        do{
+            notClean = this.$digestOnce();
+        }
+        while(notClean);
+    }
+
+}
+
+var scope = new Scope();
+scope.firstName = 'Joe';
+scope.counter = 0;
+
+scope.$watch(
+    function() {
+        return scope.counter;
+    },
+    function(newValue, oldValue, ctx) {
+        scope.counterIsTwo = (newValue === 2);
+    }
+);
+
+scope.$watch(
+    function() {
+        return scope.firstName;
+    },
+    function(newValue, oldValue, ctx) {
+        scope.counter++;
+    }
+);
+
+// After the first digest the counter is 1
+scope.$digest();
+
+//Первым аргументом может быть все, что угодно: функция, проверка на равенство
+// или проверка существования объекта.
+//и, вторым аргументом, сообщение, которое будет выведено в консоль
+// вместе с выброшенным исключением, если первое условие не будет выполнено
+console.assert(scope.counter === 1, "scope.counter !== 1"); // true
+
+console.log("scope.counter === 1 " + scope.counter);
+
+// On the next change the counter becomes two, and the other watch listener is also run because of the dirty check
+scope.firstName = 'Jane';
+console.log("scope.firstName === 1 " + scope.firstName);
+scope.$digest();
+console.assert(scope.counter === 2, "scope.counter !== 2"); // true
+
+console.log("scope.counter === 2 " + scope.counter);
+console.assert(scope.counterIsTwo, "!scope.counterIsTwo"); // true
+
+console.log("scope.counterIsTwo " + scope.counterIsTwo);
+

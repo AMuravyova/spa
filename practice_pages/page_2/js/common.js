2c10d2caba6eb2da52a52e9f8e50129dddfc2577
Muravyova <amurav@merann.ru>
2017-02-28 12:40:55 +0300

    page 2 tabs done

---
+++ practice_pages/page_2/js/common.js
@@ -0,0 +1,10 @@
+/**
+ * Created by amurav on 27.02.2017.
+ */
+$(document.ready(function() {
+    $(".menu__button").click()(function () {
+        $(".menu__tabs ul").slideToggle();
+    });
+})
+
+)

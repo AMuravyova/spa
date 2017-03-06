2435cb1bb57a2cabb86db59b630e9b2aff8a190a
Muravyova <amurav@merann.ru>
2017-03-06 11:39:29 +0300

    don't work img and text

--- practice_pages/page_2/js/common.js
+++ practice_pages/page_2/js/common.js
@@ -1,17 +1,71 @@
 /**
  * Created by amurav on 27.02.2017.
  */
+/**
+ * Created by amurav on 27.02.2017.
+ */
 $(document).ready(function() {
 
 
     $(".menu__button").click(function () {
+        //       $(".menu__tabs__toggle ul").css("visibility", "visible").slideToggle("slow");
 
-        $(".menu__tabs__toggle ul").css("visibility","visible").slideToggle("slow");
-        //$(this).next().slideToggle("slow");
-        //$(".menu__tabs__toggle").slideToggle();
+        if($(".menu__tabs__toggle ul").css("visibility")==="visible"){
+            $(".menu__tabs__toggle ul").slideToggle("slow");
+        }
+        else{
+            $(".menu__tabs__toggle ul").css("visibility", "visible");
+        }
+
+    });
+
+
+    $(".picture").click(function () {
+
+    });
+
+
+    $(".edit_btn").click(function(){
+        var ctx = this;
+        return function (){
+            var parentBlock = ctx.parentNode;
+            var listBlockChildren = parentBlock.children;
+            if(listBlockChildren[1].className === "paragrath"){
+                var listButtonChildren = listBlockChildren[0].children;
+                listButtonChildren[0].className = "glyphicon glyphicon-ok";
+
+                var paraText = listBlockChildren[1].innerHTML;
+                var isArea = document.createElement("textarea");
+                isArea.className = "textarea";
+                var node = document.createTextNode(paraText);
+                isArea.appendChild(node);
+                parentBlock.removeChild(listBlockChildren[1]);
+                parentBlock.appendChild(isArea);
+
+
+            }
+            else if(listBlockChildren[1].className === "textarea"){
+                listButtonChildren = listBlockChildren[0].children;
+                listButtonChildren[0].className = "glyphicon glyphicon-pencil";
+                var areaText = listBlockChildren[1].value;
+                var isP = document.createElement("p");
+                var node = document.createTextNode(areaText);
+                isP.appendChild(node);
+                parentBlock.removeChild(listBlockChildren[1]);
+                parentBlock.appendChild(isP);
+
+
+            }
+
+            return ctx = this;
+        }();
+
+
+
     });
 
 
 
 });
+
 
